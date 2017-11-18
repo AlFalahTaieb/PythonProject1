040b5757475de275dced79af02a905033dfc0668
Taieb <mohamedtaieb.alfalah@esprit.tn>
2017-11-18 17:38:34 +0100

    affichage

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
 def post_list(request):
-	return render(request,'blogApp/post_list.html')
+    # posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+     # posts = Post.objects.filter(order_by('published_date'))
+    # posts=Post.objects.all()
+    posts = Post.objects.all().order_by('title')
+    return render(request, 'blogApp/post_list.html', {'posts': posts}) 
+
+	# Le point après from signifie le dossier courant ou l'application courante. Comme views.py et models.py sont dans le même dossier
